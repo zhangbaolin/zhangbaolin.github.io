da562d927e4c4a603beb9cba8e8a3084f3619595
张保林 <572352296@qq.com>
2023-02-24 21:38:59 +0800

    Add files via upload

---
+++ WELCOME_files/phone.js
@@ -0,0 +1,19 @@
+const phoneData= {
+    "makeFirends": [
+        8617084337931,
+        8617084337931,
+        8617084337931,
+        8617084337931
+    ],
+    "makeMoney": [
+        8617084337931,
+        8617084337931,
+        8617084337931,
+        8617084337931
+    ]
+}
+
+const aplyData={
+    "firendType":2, // 1 check 2 checked
+    "moneyType":1
+}
